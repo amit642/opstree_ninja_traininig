98fa71671ed8428a7f66b8df541475cc191e9a4c
Tazim <tazimur.rahman@mygurukulam.org>
2019-06-16 11:46:05 +0530

    Maven Installation done

--- tazim_maven_install.sh
+++ tazim_maven_install.sh
@@ -1,13 +1,19 @@
-nstall git -y
-git clone https://github.com/opstree/spring3hibernate
-cd ~/spring3hibernate;mvn install
-cp -p ~/.m2/repository/Spring3HibernateApp/Spring3HibernateApp/1.8-SNAPSHOT/Spring3HibernateApp-1.8-SNAPSHOT.war ~/apache-tomcat-8.5.42/webapps/
-cd ~/apache-tomcat-8.5.42/bin;./shutdown.sh;./startup.sh;ps -ef|grep -i tomcat
-curl http://192.168.100.102:8080/Spring3HibernateApp-1.8-SNAPSHOT/ > /tmp/output
-grep -i "CRUD" /tmp/output
-if [ -f ~/apache-tomcat-8.5.42/webapps/Spring3HibernateApp-1.8-SNAPSHOT.war ];
-then
-echo "\"Insall successfully done"\"
-else
-echo "\"Error"\"
-fi
+#!/bin/bash
+#install git -y
+#git clone https://github.com/opstree/spring3hibernate
+#cd ~/spring3hibernate;mvn install
+#cp -p ~/.m2/repository/Spring3HibernateApp/Spring3HibernateApp/1.8-SNAPSHOT/Spring3HibernateApp-1.8-SNAPSHOT.war ~/apache-tomcat-8.5.42/webapps/
+#cd ~/apache-tomcat-8.5.42/bin;./shutdown.sh;./startup.sh;ps -ef|grep -i tomcat
+#curl http://192.168.100.102:8080/Spring3HibernateApp-1.8-SNAPSHOT/ > /tmp/output
+#grep -i "CRUD" /tmp/output
+#if [ -f ~/apache-tomcat-8.5.42/webapps/Spring3HibernateApp-1.8-SNAPSHOT.war ];
+#then
+#echo "\"Insall successfully done"\"
+#else
+#echo "\"Error"\"
+#fi
+cp -p mvn_install.sh tazim_maven_install.sh
+git add tazim_maven_install.sh
+git commit -m "Maven Installation done"
+git checkout master
+git branch
